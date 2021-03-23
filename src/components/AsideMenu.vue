faffffcd1903d48f2de7ebda10b760e13692f6e3
Raindot <sakuhime24@gmail.com>
2021-03-23 16:15:14 +0800

    adjust detail

--- src/components/AsideMenu.vue
+++ src/components/AsideMenu.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="bg-light">
-    <div class="text-center py-4">口罩即時查</div>
+  <div class="bg-light aside-menu">
+    <h4 class="text-center py-3">口罩即時查</h4>
     <div class="px-2">
       <label class="form-label">
         縣市：
@@ -20,38 +20,36 @@
       <label class="form-label">
         <div class="py-2 pl-4"> <i class="fas fa-search-location"></i> 藥局名稱搜尋：</div>
       </label>
-        <input class="form-control" v-model="keywords" type="text" placeholder="請輸入關鍵字">
+      <input class="form-control" v-model="keywords" type="text" placeholder="請輸入關鍵字">
     </div>
-      <div class="card my-3 mx-2"
-        v-for="store in filteredStores"
-        :key="store.id"
-        @click="$emit('triggerMarkerPopup', store.id)">
-        <div class="card-body">
-          <div class="row gx-1 text-white flex-nowrap">
-            <div class="col mask-count adult-mask rounded-3 me-3 px-3 py-3">
-              <span>大人口罩</span>
-              <div class="fs-3 text-nowrap">{{ store.mask_adult }} <span class="fs-5">個</span></div>
-            </div>
-            <div class="col mask-count child-mask rounded-3 px-3 py-3">
-              <span>兒童口罩</span>
-              <div class="fs-3 text-nowrap">{{ store.mask_child }} <span class="fs-5">個</span></div>
-            </div>
+    <div class="store card my-3 mx-2" v-for="store in filteredStores" :key="store.id"
+      @click="$emit('triggerMarkerPopup', store.id)">
+      <div class="card-body">
+        <div class="row gx-1 text-white flex-nowrap">
+          <div class="col mask-count adult-mask rounded-3 me-3 px-3 py-3">
+            <span>大人口罩</span>
+            <div class="fs-3 text-nowrap">{{ store.mask_adult }} <span class="fs-5">個</span></div>
           </div>
-          <h5 class="card-title py-3" v-html="keywordHighlight(store.name)"></h5>
-          <div>
-            地址： {{store.address}}
-          </div>
-          <div>
-            電話： {{store.phone}}
-          </div>
-          <div>
-            最後更新時間: {{ store.updated }}
-          </div>
-          <div>
-            {{store.custom_note}}
+          <div class="col mask-count child-mask rounded-3 px-3 py-3">
+            <span>兒童口罩</span>
+            <div class="fs-3 text-nowrap">{{ store.mask_child }} <span class="fs-5">個</span></div>
           </div>
         </div>
+        <h5 class="card-title py-3" v-html="keywordHighlight(store.name)"></h5>
+        <div>
+          地址： {{store.address}}
+        </div>
+        <div>
+          電話： {{store.phone}}
+        </div>
+        <div>
+          最後更新時間: {{ store.updated }}
+        </div>
+        <div>
+          {{store.custom_note}}
+        </div>
       </div>
+    </div>
   </div>
 </template>
 
@@ -106,6 +104,13 @@ export default {
 </script>
 
 <style lang="scss">
+  .aside-menu {
+    overflow: scroll;
+    height: 100vh;
+  }
+  .store:hover {
+    cursor: pointer;
+  }
   .highlight {
     color: #f08d69;
   }
